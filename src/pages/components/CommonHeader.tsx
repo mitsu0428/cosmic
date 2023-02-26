ad91299cb9bc14343bf04241d0e01761a4538c39
mitsu0428 <mitsuprograming@gmail.com>
2023-02-26 21:48:33 +0900

    iconサイズを画面に応じて変更する”する”

--- src/pages/components/CommonHeader.tsx
+++ src/pages/components/CommonHeader.tsx
@@ -3,21 +3,40 @@ import styled from "styled-components";
 import { FaTwitter, FaInstagram, FaYoutube, FaDiscord } from "react-icons/fa";
 
 const CommonHeader = () => {
+  const [iconSize, setIconSize] = React.useState(35);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width <= 768) {
+        setIconSize(24);
+      } else if (width <= 1024) {
+        setIconSize(32);
+      } else {
+        setIconSize(35);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
     <Header>
       <Title>Cosmictheta</Title>
       <SnsCOntainer>
         <Sns>
-          <FaTwitter size={35} color={"#ccc"} />
+          <FaTwitter size={iconSize} color={"#ccc"} />
         </Sns>
         <Sns>
-          <FaInstagram size={35} color={"#ccc"} />
+          <FaInstagram size={iconSize} color={"#ccc"} />
         </Sns>
         <Sns>
-          <FaYoutube size={35} color={"#ccc"} />
+          <FaYoutube size={iconSize} color={"#ccc"} />
         </Sns>
         <Sns>
-          <FaDiscord size={35} color={"#ccc"} />
+          <FaDiscord size={iconSize} color={"#ccc"} />
         </Sns>
       </SnsCOntainer>
     </Header>
@@ -48,11 +67,26 @@ const SnsCOntainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-left: 16px;
+  @media (max-width: 480px) {
+    margin-left: 8px;
+  }
 `;
 
 const Sns = styled.div`
   margin-left: 16px;
   margin-right: 16px;
+  @media (max-width: 480px) {
+    margin-left: 8px;
+    margin-right: 8px;
+  }
+  svg {
+    width: 35px;
+    height: 35px;
+    @media (max-width: 480px) {
+      width: 25px;
+      height: 25px;
+    }
+  }
 `;
 
 export default CommonHeader;
